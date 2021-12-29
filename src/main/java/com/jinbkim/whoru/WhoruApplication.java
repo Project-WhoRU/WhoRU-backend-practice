f12cf709df09e7437cea4060f3c0f121ddfb00c3
jinbkim <topmedia3@naver.com>
2021-12-29 12:26:30 +0900

    ADD spring boot setting

---
+++ src/main/java/com/jinbkim/whoru/WhoruApplication.java
@@ -0,0 +1,13 @@
+package com.jinbkim.whoru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinbkim/whoru/WhoruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinbkim.whoru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhoruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
